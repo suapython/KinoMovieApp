6012e7ad3e711c2d103cdd8a045abec976a01d20
suapython <morales@roma2.infn.it>
2020-05-25 19:25:58 +0200

    Initial Commit

---
+++ KinoMovieApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KinoMovieApp
+//
+//  Created by jose francisco morales on 25/05/2020.
+//  Copyright © 2020 jose francisco morales. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
